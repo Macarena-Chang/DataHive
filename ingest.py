c1b9a79e6417a1cb374fd20b9b6849b032b93e85
macarena-chang <changmacarena@gmail.com>
2023-04-21 09:56:46 +0200

    refactor: separate code into functions for organization and legibility

--- ingest.py
+++ ingest.py
@@ -5,64 +5,66 @@ import uuid
 import json
 import yaml
 
-# Read configurations from YAML file
-with open("config.yaml", "r") as config_file:
-    config = yaml.safe_load(config_file)
 
-# Set up API keys and configurations
-openai_key = config["openai_key"]
-pinecone_api_key = config["pinecone_api_key"]
-pinecone_environment = config["pinecone_environment"]
-pinecone_index_name = config["pinecone_index_name"]
+def load_config(file_path: str) -> dict:
+    with open(file_path, "r") as config_file:
+        return yaml.safe_load(config_file)
 
-# Configure OpenAI and Pinecone
-openai.api_key = openai_key
-pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
 
-# Read text from a txt file and save it to a variable called "my_string"
-file_path = "Python-Data-Structures.txt"
-with open(file_path, "r") as file:
-    my_string = file.read()
-my_string.replace('\n', ' ')
-my_string.replace('\\n', ' ')
+def read_text_file(file_path: str) -> str:
+    with open(file_path, "r") as file:
+        text = file.read()
+    return text.replace('\n', ' ').replace('\\n', ' ')
 
-# Split text of "my_string" variable into chunks and save the chunks to "my_chunks"
-char_text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-my_chunks = char_text_splitter.split_text(my_string)
 
-# Generate a unique ID for the file
-file_unique_id = str(uuid.uuid4())
+def split_text_data(text: str) -> list:
+    # Ssplit text into chunks 
+    char_text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    return char_text_splitter.split_text(text)
 
-# Initialize Pinecone vector store
-pinecone_store = pinecone.Index(pinecone_index_name)
+def generate_embeddings(chunks: list) -> list:
+    embeddings = []
+    for chunk in chunks:
+        response = openai.Embedding.create(input=chunk, model="text-embedding-ada-002")
+        embeddings.append(response["data"][0]["embedding"])
+    return embeddings
 
-# create a mapping between vector ids and original text chunks
-id_to_text_mapping = {}
 
-# store embeddings of the chunks with their unique ids in pinecone
-for idx, chunk in enumerate(my_chunks):
-    # generate a unique ID for each chunk
-    chunk_unique_id = f"{file_unique_id}_{idx}"
+def store_embeddings(chunks: list, embeddings: list, file_unique_id: str) -> dict:
+    id_to_text_mapping = {}
+    for idx, (chunk, embedding) in enumerate(zip(chunks, embeddings)):
+        chunk_unique_id = f"{file_unique_id}_{idx}"
+        metadata = {'chunk': idx, 'text': chunk, 'file_id': file_unique_id}
+        id_to_text_mapping[chunk_unique_id] = metadata
+        pinecone_store.upsert(vectors=zip([chunk_unique_id], [embedding], [metadata]))
+    return id_to_text_mapping
 
-    # get the embedding for the chunk with openai
-    response = openai.Embedding.create(input=chunk, model="text-embedding-ada-002")
-    embedding = response["data"][0]["embedding"]
 
-    # create metadata for the chunk
-    metadata = {
-        'chunk': idx,
-        'text': chunk,
-        'file_id': file_unique_id
-    }
+def save_mapping_to_file(mapping: dict, file_name: str):
+    with open(file_name, "w") as outfile:
+        json.dump(mapping, outfile)
 
-    # add the unique id and corresponding metadata to the mapping
-    id_to_text_mapping[chunk_unique_id] = metadata
 
-    # store the embedding and metadata in Pinecone using zip
-    pinecone_store.upsert(vectors=zip([chunk_unique_id], [embedding], [metadata]))
+if __name__ == "__main__":
+    config = load_config("config.yaml")
 
-# save the id_to_text_mapping to a JSON file
-with open(f"{file_unique_id}.json", "w") as outfile:
-    json.dump(id_to_text_mapping, outfile)
+    openai_key = config["openai_key"]
+    pinecone_api_key = config["pinecone_api_key"]
+    pinecone_environment = config["pinecone_environment"]
+    pinecone_index_name = config["pinecone_index_name"]
 
-print(id_to_text_mapping)
+    openai.api_key = openai_key
+    pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+
+    file_path = "python9-classes.txt"
+    my_string = read_text_file(file_path)
+    my_chunks = split_text_data(my_string)
+
+    file_unique_id = str(uuid.uuid4())
+    pinecone_store = pinecone.Index(pinecone_index_name)
+
+    embeddings = generate_embeddings(my_chunks)
+    id_to_text_mapping = store_embeddings(my_chunks, embeddings, file_unique_id)
+
+    save_mapping_to_file(id_to_text_mapping, f"{file_unique_id}.json")
+    print(id_to_text_mapping)
--- retrieve.py
+++ retrieve.py
@@ -1,67 +1,66 @@
 import pinecone
 import openai
-import json
 import yaml
 
-# Read configurations from YAML file
-with open("config.yaml", "r") as config_file:
-    config = yaml.safe_load(config_file)
+def load_config(file_path: str) -> dict:
+    # load config from yaml
+    with open(file_path, "r") as config_file:
+        return yaml.safe_load(config_file)
 
-# Set up API keys and configurations
-openai_key = config["openai_key"]
-pinecone_api_key = config["pinecone_api_key"]
-pinecone_environment = config["pinecone_environment"]
-pinecone_index_name = config["pinecone_index_name"]
+def configure_openai(api_key: str):
+    # config openai with api key
+    openai.api_key = api_key
 
-# Configure OpenAI
-openai.api_key = openai_key
+def initialize_pinecone(api_key: str, environment: str):
+    # Iniciar Pinecone using api key and env
+    pinecone.init(api_key=api_key, environment=environment)
 
-# Initialize Pinecone
-pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+def get_embedding(text: str, model: str = "text-embedding-ada-002"):
+    # get embedding for input text using openai ada
+    response = openai.Embedding.create(input=text, model=model)
+    return response["data"][0]["embedding"]
 
-# Initialize Pinecone vector store
-pinecone_store = pinecone.Index(index_name=pinecone_index_name)
+def query_pinecone(index, query_embedding, top_k=5, include_metadata=True):
+    # query pinecone and get relevant contexts
+    response = index.query(query_embedding, top_k=top_k, include_metadata=include_metadata)
+    return response
 
-index = pinecone.Index(pinecone_index_name)
-# index.describe_index_stats()
+def get_response_texts(response):
+    # extract response texts from result
+    return [match["metadata"]["text"] for match in response["matches"]]
 
-# Define a search query as a string
-# search_query = "Dictionaries"
-# search_query = "Manual String Formatting"
-search_query = "How to handle selected exceptions"
+def generate_summary(prompt: str):
+    # generate summary
+    response_chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response_chat.choices[0].message['content']
 
-# Get the embedding for the query using OpenAI API
-response = openai.Embedding.create(input=search_query, model="text-embedding-ada-002")
-query_embeds = response["data"][0]["embedding"]
+if __name__ == "__main__":
+    config = load_config("config.yaml")
+    configure_openai(config["openai_key"])
+    initialize_pinecone(config["pinecone_api_key"], config["pinecone_environment"])
 
-# get relevant contexts (including the questions)
-response = index.query(query_embeds, top_k=5, include_metadata=True)
+    index = pinecone.Index(config["pinecone_index_name"])
 
-# store text from responses
-response_texts = [match["metadata"]["text"] for match in response["matches"]]
+    search_query = "Multiple Inheritance in Python"
+    query_embeds = get_embedding(search_query)
+    response = query_pinecone(index, query_embeds)
+    response_texts = get_response_texts(response)
 
-print(response._data_store['matches'][0]._data_store['metadata']['text'])
-# print(response._data_store['matches'][0]._data_store['id'])
+    combined_text = " ".join(response_texts)
 
-print(response_texts)
+    prompt = f"""
+    I have gathered some relevant information to help answer your question. Here is the information:
 
-combined_text = " ".join(response_texts)
+    {combined_text}
 
-prompt = f"""
-I have gathered some relevant information to help answer your question. Here is the information:
+    Based on this information,  provide a concise summary for topic {search_query}
+    """
 
-{combined_text}
-
-Based on this information,  provide a concise summary for topic {search_query}
-"""
-
-response_chat = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt}
-    ]
-)
-
-# response text
-print(response_chat)
+    summary = generate_summary(prompt)
+    print(summary)
